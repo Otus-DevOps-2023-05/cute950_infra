1fb6ed36bdef3ccccb5bf6c62dbf86276c897c61
cute950 <cute884365@mail.ru>
2023-07-05 21:52:32 +0400

    [packer-base] Сборка образов VM при помощи Packer

--- config-scripts/install_ruby.sh
+++ config-scripts/install_ruby.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 apt update
-# иначе падает ошибка по lock-frontend
-echo "sleep 3m for install updates"; sleep 3m; echo "start install ruby"
 apt install -y ruby-full ruby-bundler build-essential
