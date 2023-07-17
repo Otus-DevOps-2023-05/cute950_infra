5019bd2d3e122a698dc531bb0add5cb1a7fa435e
cute950 <cute884365@mail.ru>
2023-07-17 12:47:21 +0400

    [terraform-1] Практика IaC с использованием Terraform

---
+++ terraform/lb.tf
@@ -0,0 +1,41 @@
+resource "yandex_lb_target_group" "reddit_app_group" {
+  count = var.count
+  name = "reddit-app-group-${count.index}"
+  region_id = "ru-central1"
+  folder_id = var.folder_id
+
+  dynamic "target" {
+    for_each = yandex_compute_instance.app
+    content {
+      subnet_id = var.subnet_id
+      address   = target.value.network_interface.0.ip_address
+    }
+  }
+}
+
+resource "yandex_lb_network_load_balancer" "reddit_app_balancer" {
+  name = "reddit-app-balancer"
+  region_id = "ru-central1"
+  folder_id = var.folder_id
+
+  listener {
+    name = "reddit-app-listener"
+    port = 8080
+    target_port = 9292
+    external_address_spec {
+      ip_version = "ipv4"
+    }
+  }
+
+  attached_target_group {
+    target_group_id = yandex_lb_target_group.reddit_app_group.id
+
+    healthcheck {
+      name = "http"
+      http_options {
+        port = 9292
+        path = "/"
+      }
+    }
+  }
+}
