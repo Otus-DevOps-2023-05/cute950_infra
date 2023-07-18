d732bac59974096239b89e3961261488a0fd01f1
cute950 <cute884365@mail.ru>
2023-07-18 23:31:17 +0400

    [terraform-2] Принципы организации инфраструктурного кода и работа над инфраструктурой в команде на примере Terraform.

---
+++ terraform/variables.tf
@@ -0,0 +1,28 @@
+variable "cloud_id" {
+  description = "cloud_id"
+}
+
+variable "folder_id" {
+  description = "folder_id"
+}
+
+variable "zone" {
+  description = "zone"
+  default     = "ru-central1-a"
+}
+
+variable "token" {
+  description = "token"
+}
+
+variable "access_key" {
+  description = "key id"
+}
+
+variable "secret_key" {
+  description = "secret key"
+}
+
+variable "bucket_name" {
+  description = "bucket name"
+}
