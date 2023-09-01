6467583872212ed389d96644c36d63f7f80f4c14
cute950 <cute884365@mail.ru>
2023-09-01 19:13:28 +0400

    [ansible-2] Деплой и управление конфигурацией с Ansible

--- terraform/modules/app/main.tf
+++ terraform/modules/app/main.tf
@@ -42,12 +42,12 @@ resource "yandex_compute_instance" "app" {
     agent       = false
     private_key = file(var.private_key_path)
   }
-  provisioner "file" {
-    content     = templatefile("${path.module}/files/puma.service", { db_ip = var.db_ip })
-    destination = "/tmp/puma.service"
-  }
+  # provisioner "file" {
+  #   content     = templatefile("${path.module}/files/puma.service", { db_ip = var.db_ip })
+  #   destination = "/tmp/puma.service"
+  # }
 
-  provisioner "remote-exec" {
-    script = "${path.module}/files/deploy.sh"
-  }
+  # provisioner "remote-exec" {
+  #   script = "${path.module}/files/deploy.sh"
+  # }
 }
